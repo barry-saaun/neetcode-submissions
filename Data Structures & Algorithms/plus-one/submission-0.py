a895ed1e675df12471d3dfb45991bdac8f95bcef
Barry Saaun <panhaocr@gmail.com>
2026-08-04 18:47:15 +1000

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        total = 0
+        for i in range(len(digits)):
+            total += digits[i] *  (10 ** (len(digits) - i - 1))
+        
+        return self.numberToDigits(total + 1)
+
+    def numberToDigits(self, n: int) -> List[int]:
+        if n == 0:
+            return [0]
+        
+        digits = []
+        while n > 0:
+            digits.append(n % 10)
+            n //= 10
+
+        return digits[::-1]
+
